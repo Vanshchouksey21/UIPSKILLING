279e84a10adff4f4e4fcebccd31a985a9a703e55
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-07-26 18:07:15 +0530

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import Table from './table.jsx'
-import Abc from './abc.jsx'
 
 const App = () => {
   return (
-    <div>
-      
-      <Table/>
-     {/* <Abc/> */}
-    </div>
+    <div>App</div>
   )
 }
 
